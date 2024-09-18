85b2d7da5325b78c07a63bd5a99f5bac50ecd9ad
Cats-R-Cool <38488@ma-web.nl>
2024-09-18 16:11:46 +0200

    Initial commit

---
+++ reken.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 200;
+let c = 15;
+
+if (a>b) {
+    console.log("a is bigger then b");
+} else if(a<b){
+    console.log("b is bigger then a");
+} else if(a==c || b==c){
+    console.log("whoah there is another option?!");
+} else {
+    console.log("neither are true???");
+}
---
+++ school.js
@@ -0,0 +1,10 @@
+var d = new Date();
+d.getDay();
+if(d == "6"){
+    console.log("weekend!!");
+} if(d == "0"){
+    console.log("weekend!!");
+}
+else {
+    console.log("Ik moet naar school");
+}
